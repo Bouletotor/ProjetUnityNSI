8a7ed9b29edd649697314e992ef044fb6b0bad4c
Bouletotor <bouletotor@gmail.com>
2021-05-03 18:42:29 +0200

    Ajout d'un saut (en cours de développement)

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,16 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour 
 {
     public float moveSpeed;
+    public float jumpForce;
+    private float jumpNumber = 2;
 
-    public Rigidbody2D rb;
-    private Vector3 velocity = Vector3.zero;
+    public bool isJumping;
+
+    public Transform groundCheckerLeft;
+    public Transform groundCheckerRight;
+
+    public Rigidbody2D rb; 
+    private Vector3 velocity = Vector3.zero; 
 
     void FixedUpdate()
     {
-        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime; 
+
+        if (Input.GetButtonDown("Jump") && jumpNumber > 0)
+        {
+            isJumping = true;
+            jumpNumber -= 1;
+        }
 
         MovePlayer(horizontalMovement);
     }
@@ -19,6 +32,12 @@ public class PlayerMovement : MonoBehaviour
     void MovePlayer(float _horizontalMovement)
     {
         Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
-        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, 0.05f);
+
+        if (isJumping == true) 
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+        }
     }
 }

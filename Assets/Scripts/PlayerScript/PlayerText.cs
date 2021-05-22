56e29d0c9b0c5f26a90a82a1694ae385658c0e5a
SaskaTheProg <81176441+SaskaTheProg@users.noreply.github.com>
2021-05-22 09:13:15 +0200

    commentaire playetext

--- Assets/Scripts/PlayerScript/PlayerText.cs
+++ Assets/Scripts/PlayerScript/PlayerText.cs
@@ -2,32 +2,32 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerText : MonoBehaviour
-{
-    public LayerMask collisionLayers;
-    private string texte = "";
+{   
+    //Créer une variable texte dans un interface
     public Text texteUI;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
+        //fonction qui qui s'execute quand le joueur entre en collision
+        //Détecte le nom de l'objet dans lequel le joueur entre collision pour afficher un texte différent
+        //Tuto1,2 et 3 corresponde aux différents panneaux du tutoriel
         if(collision.gameObject.name == "Tuto1")
         {
-            texte = "Appuyez sur [q] ou [d] pour vous deplacez de gauche a droite";
+            texteUI.text = "Appuyez sur [q] ou [d] pour vous deplacez de gauche a droite";
         }
         else if(collision.gameObject.name == "Tuto2")
         {
-            texte = "Appuyez sur la touche [Espace] pour sauter et ainsi franchir l'obstacle";
+            texteUI.text = "Appuyez sur la touche [Espace] pour sauter et ainsi franchir l'obstacle";
         }
         else if (collision.gameObject.name == "Tuto3")
         {
-            texte = "Voici votre premier ennemie, appuyez sur [Clic gauche] pour l'attaquer";
+            texteUI.text = "Voici votre premier ennemie, appuyez sur [Clic gauche] pour l'attaquer";
         }
-        texteUI.text = texte;
     }
 
     void OnTriggerExit2D(Collider2D collision)
     {
-        texte = "";
-        texteUI.text = texte;
+        texteUI.text = "";
     }
     
 }

e28f026f01fc687f276d7b12ba361d69d573956a
Bouletotor <bouletotor@gmail.com>
2021-05-25 16:32:47 +0200

    Update EnemyJump.cs

--- Assets/Scripts/EnemyJump.cs
+++ Assets/Scripts/EnemyJump.cs
@@ -1,38 +1,40 @@
+// Script permettant aux ennemis de sauter
+
+// Importation de modules propres à Unity
 using System.Collections;
 using UnityEngine;
 
 public class EnemyJump : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public bool canJump;
+    public Rigidbody2D rb; // Variable relative à la physique de l'ennemi
+    public float jumpForce; // Force du saut
 
-    public float jumpForce;
-
+    public bool canJump; 
     private bool isJumpIntervalRunning = false;
 
     private void Update()
     {
         if(canJump == true)
         {
-            Jump(jumpForce);
+            Jump(jumpForce); // On appelle la fonction Jump() définie plus loin
         }
         else if(isJumpIntervalRunning == false)
         {
-            StartCoroutine(JumpInterval());
+            StartCoroutine(JumpInterval()); // On appelle la coroutine JumpInterval() définie plus loin
         }
     }
 
     private void Jump(float jumpForce)
     {
-        rb.AddForce(new Vector2(0f, jumpForce));
-        canJump = false;
+        rb.AddForce(new Vector2(0f, jumpForce)); // On ajoute au déplacement de l'ennemi (traité dans une autre fonction) un force verticale
+        canJump = false; // On bloque le saut
     }
 
-    IEnumerator JumpInterval()
+    IEnumerator JumpInterval() 
     {
-        isJumpIntervalRunning = true;
-        yield return new WaitForSeconds(1.8f);
-        canJump = true;
-        isJumpIntervalRunning = false;
+        isJumpIntervalRunning = true; // On bloque l'appel à la coroutine
+        yield return new WaitForSeconds(1.8f); // On attends 1.8 secondes
+        canJump = true; // On permet à l'ennemi de sauter
+        isJumpIntervalRunning = false; // On permet l'appel à la coroutine
     }
 }

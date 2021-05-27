c7f51075814a354c93b5eef8a610c6df06fe77e3
SaskaTheProg <81176441+SaskaTheProg@users.noreply.github.com>
2021-05-27 20:25:29 +0200

    Commentaires

--- Assets/Scripts/PNJInteraction.cs
+++ Assets/Scripts/PNJInteraction.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class PNJInteraction : MonoBehaviour
 {
+    //Définis une variable de type Item qui, en l'occurence, va être le seul objet du jeu
     public Item amulette;
-
+    
+    //Fonction qui s'éxecute lorsque le pnj entre en collision
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && Inventory.instance.content.Contains(amulette) == false)

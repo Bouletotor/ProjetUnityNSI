a6b443952c4272113574d1dcd9b6c96ec26d42e7
PhoenixTheProgdu92 <83463962+PhoenixTheProgdu92@users.noreply.github.com>
2021-05-28 17:22:51 +0200

    commentateur pro

--- Assets/Scripts/DontDestroyOnLoadScene.cs
+++ Assets/Scripts/DontDestroyOnLoadScene.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 public class DontDestroyOnLoadScene : MonoBehaviour
 {
 
+    //Ici la variable va prendre en compte certains éléments du niveau comme la vie du personnage
+    //Et la conserver lors d'un changement de niveau. On garde les éléments qui sont sensé rester les mêmes lors d'un changement de scène
+    //Et on ne conserve pas ceux qui doivent être remplacés comme le Grid.
     public GameObject[] objects;
 
     void Awake()
